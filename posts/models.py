97d439a955fc07c7f2abd49ad20d136693afdfc3
Samandar <samandarnayimjonov@gmail.com>
2025-02-21 21:12:50 +0500

    Post modeliga status fieldi qushildi

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150)
     slug = models.SlugField(unique=True)
     content = models.TextField()
+    status = models.CharField(max_length=50)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
     tags = models.ManyToManyField(Tags, related_name='posts')
