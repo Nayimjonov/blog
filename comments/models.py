fdb7c4e6f196f68ecf445619552fd9c3c0dd4f6b
Samandar <samandarnayimjonov@gmail.com>
2025-02-21 21:22:10 +0500

    Comment modeliga created_at fieldi qushildi

--- comments/models.py
+++ comments/models.py
@@ -3,7 +3,8 @@ from posts.models import Post
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author_name = models.CharField(max_length=100)
     author_email = models.EmailField()
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
