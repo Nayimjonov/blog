7f11e4f4d76229ee86795afe21237b30893ebe23
Samandar <samandarnayimjonov@gmail.com>
2025-02-21 21:25:03 +0500

    update Post modele

--- posts/models.py
+++ posts/models.py
@@ -10,9 +10,8 @@ class Post(models.Model):
     slug = models.SlugField(unique=True)
     content = models.TextField()
     status = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
     tags = models.ManyToManyField(Tag, related_name='posts')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
