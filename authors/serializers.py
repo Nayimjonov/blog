830a797a10fd91612a7562f9a3658d36257e6c42
Samandar <samandarnayimjonov@gmail.com>
2025-02-21 22:09:33 +0500

    author serializerda barcha fieldlarga validatsiya yozildi

--- authors/serializers.py
+++ authors/serializers.py
@@ -6,3 +6,18 @@ class AuthorModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ('id', 'name', 'email', 'bio')
+
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Muallif ismi kamida 2 ta belgidan iborat bo'lishi kerak.")
+        return value
+
+    def validate_email(self, value):
+        if Author.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Bu email manzil bilan ro'yxatdan o'tilgan.")
+        return value
+
+    def validate_bio(self, value):
+        if value and len(value.split()) < 5:
+            raise serializers.ValidationError("Biografiya kamida 5 ta so'zdan iborat bo'lishi kerak.")
+        return value
