46443209870d917e2bff40ebe36fb4984ce94ee1
Samandar <samandarnayimjonov@gmail.com>
2025-02-21 21:34:56 +0500

    Comment modeliga str sehrli method qushildi

--- comments/models.py
+++ comments/models.py
@@ -9,3 +9,6 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     parent_comment = models.CharField(max_length=100)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return f"{self.author_name}"
