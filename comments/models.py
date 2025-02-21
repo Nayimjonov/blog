cc405825bc66d8b44cf08a1ac3b9ae41f235f960
Samandar <samandarnayimjonov@gmail.com>
2025-02-21 21:18:18 +0500

    Comment modeliga post fieldi yozildi

--- comments/models.py
+++ comments/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from posts.models import Post
 
 
 class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from authors.models import Author
 from categories.models import Category
+from tags.models import Tag
 
 
 class Post(models.Model):
@@ -11,7 +12,7 @@ class Post(models.Model):
     status = models.CharField(max_length=50)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
-    tags = models.ManyToManyField(Tags, related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
