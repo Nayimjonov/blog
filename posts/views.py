f3e3ba0e50e195727066b34ec8d1b8865adbab76
Samandar <samandarnayimjonov@gmail.com>
2025-02-22 00:44:41 +0500

    put method done

--- posts/views.py
+++ posts/views.py
@@ -31,6 +31,15 @@ class PostDetailAPIView(APIView):
         serializer = PostModelSerializer(post)
         return Response(serializer.data)
 
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostModelSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
 
