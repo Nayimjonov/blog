2b0fc0186a0734e341236afae9e994d3b833ec15
Samandar <samandarnayimjonov@gmail.com>
2025-02-21 22:27:56 +0500

    AuthorAPIView get methodi yozibolindi

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Author
+from .serializers import AuthorModelSerializer
 
-# Create your views here.
+
+class AuthorAPIView(APIView):
+    def get(self, request):
+        authors = Author.objects.all()
+        serialize = AuthorModelSerializer(authors, many=True)
+        return Response(serialize.data)
