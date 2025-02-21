e2485e0ddb858c99ecffe946ef4e2ca4b1bf8f56
Samandar <samandarnayimjonov@gmail.com>
2025-02-21 21:19:29 +0500

    Comment modeliga authon_name fieldi qushildi

--- comments/models.py
+++ comments/models.py
@@ -4,3 +4,4 @@ from posts.models import Post
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author_name = models.CharField(max_length=100)
