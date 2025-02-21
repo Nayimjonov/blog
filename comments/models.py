38f331b98e53ec5aac8dfcd921db8dd14516d051
Samandar <samandarnayimjonov@gmail.com>
2025-02-21 21:20:12 +0500

    Comment modeliga author_email fieldi qushildi

--- comments/models.py
+++ comments/models.py
@@ -5,3 +5,4 @@ from posts.models import Post
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author_name = models.CharField(max_length=100)
+    author_email = models.EmailField()
