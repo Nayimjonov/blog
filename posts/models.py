6f205f83a744d1fc9db5fc4ad8c135b16a186c50
Samandar <samandarnayimjonov@gmail.com>
2025-02-21 21:03:57 +0500

    Post modeliga author fieldi qushildi

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+from authors.models import Author
+
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
     slug = models.SlugField(unique=True)
     content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
+
