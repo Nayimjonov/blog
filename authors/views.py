ac512ed20583455a7b146f4340cb8de750d2ae74
Samandar <samandarnayimjonov@gmail.com>
2025-02-21 22:40:00 +0500

    AuthorDetailView get_object method yozib tugatildi

--- authors/views.py
+++ authors/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Author
@@ -17,3 +18,12 @@ class AuthorAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+class AuthorDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            raise Http404
+
+
