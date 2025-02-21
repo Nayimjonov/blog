4e1ef2a847ec353cd35a3916880dc9cdf429a29e
Samandar <samandarnayimjonov@gmail.com>
2025-02-22 00:06:47 +0500

    tag serializer ham qushildi

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Tag
 
 
 class PostModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', '')
+        fields = (
+            'id', 'title', 'slug', 'content', 'status',
+            'created_at', 'updated_at', 'author',
+            'category', 'tags'
+        )
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name', 'slug')
