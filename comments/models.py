ebea8e82018c4fca1a1d3c34e0bd510c3b167eb0
Samandar <samandarnayimjonov@gmail.com>
2025-02-21 21:23:04 +0500

    Comment modeliga parent_comment fieldi qushildi

--- comments/models.py
+++ comments/models.py
@@ -7,4 +7,5 @@ class Comment(models.Model):
     author_email = models.EmailField()
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    parent_comment = models.CharField(max_length=100)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
