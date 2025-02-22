1f0831a35bb2084afbce5b17cc0b71b3bc4ef0a5
Samandar <samandarnayimjonov@gmail.com>
2025-02-22 13:15:18 +0500

    post method done

--- comments/views.py
+++ comments/views.py
@@ -12,5 +12,11 @@ class CommentAPIView(APIView):
         serializer = CommentModelSerializer(comments, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CommentModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
