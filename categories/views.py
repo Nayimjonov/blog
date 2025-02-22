76f047233a3c3b6b7ffdfa010a717c6fbb1a5688
Samandar <samandarnayimjonov@gmail.com>
2025-02-22 12:34:36 +0500

    CategoryAPIView ichida get methodi yaratildi

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Category
+from .serializers import CategoryModelSerializer
 
-# Create your views here.
+
+class CategoryAPIView(APIView):
+    def get(self, request):
