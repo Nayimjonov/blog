7bef40f99fe76f4bb00effd21a3a0741edb5db89
Samandar <samandarnayimjonov@gmail.com>
2025-02-22 12:28:51 +0500

    Category serializer yozib tugatildi

---
+++ categories/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Category
+
+
+class CategoryModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'slug', 'description')
