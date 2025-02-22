b22bdfe60a40d23f50ba7b2692bd2105699e978c
Samandar <samandarnayimjonov@gmail.com>
2025-02-22 12:37:49 +0500

    CategoryAPIView da post method all done

--- categories/views.py
+++ categories/views.py
@@ -12,3 +12,10 @@ class CategoryAPIView(APIView):
         serializer = CategoryModelSerializer(categories, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CategoryModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
