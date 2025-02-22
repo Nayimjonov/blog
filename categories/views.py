a3ded169e6453762534ffbcce548056fb6b5d52d
Samandar <samandarnayimjonov@gmail.com>
2025-02-22 12:48:30 +0500

    done delete method

--- categories/views.py
+++ categories/views.py
@@ -39,7 +39,10 @@ class CategoryDetailAPIView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self):
+    def delete(self, request, pk):
+        category = self.get_object(pk)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
