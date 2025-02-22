991b880b2d7915f9dd72734a0b7535f555754df7
Samandar <samandarnayimjonov@gmail.com>
2025-02-22 13:01:45 +0500

    all done serializer in comments app

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,3 +3,9 @@ from .models import Comment
 
 
 class CommentModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = (
+            'id', 'author_name', 'author_email', 'content',
+            'created_at', 'parent_comment', 'post'
+        )
