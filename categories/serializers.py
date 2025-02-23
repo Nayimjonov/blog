5df6b3cf666f6a4d475b0074ff014e07dea794b9
Samandar <samandarnayimjonov@gmail.com>
2025-02-23 17:25:29 +0500

    category serializer ga validatsiya yozildi fieldlarga

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from .models import Category
+from django.utils.text import slugify
+
 
 
 class CategoryModelSerializer(serializers.ModelSerializer):
+    post_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
-        fields = ('id', 'name', 'slug', 'description')
+        fields = ('id', 'name', 'slug', 'description', 'post_count')
+
+    def get_post_count(self, obj):
+        return obj.posts.count()
+
+    def create(self, validated_data):
+        validated_data['slug'] = slugify(validated_data['name'])
+        return super().create(validated_data)
