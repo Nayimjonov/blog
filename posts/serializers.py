5365d535f458808c56d9870903bd87f9141c8501
Samandar <samandarnayimjonov@gmail.com>
2025-02-23 17:28:35 +0500

    postmodelserializer udpate

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from authors.serializers import AuthorModelSerializer
 from .models import Post, Tag
 from django.utils.text import slugify
+from categories.serializers import CategoryModelSerializer
 
 
 class TagModelSerializer(serializers.ModelSerializer):
@@ -21,12 +22,17 @@ class TagModelSerializer(serializers.ModelSerializer):
 
 class PostModelSerializer(serializers.ModelSerializer):
     author = AuthorModelSerializer()
+    category = CategoryModelSerializer()
     tags = TagModelSerializer(many=True)
+    comments_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = (
-            'id', 'title', 'slug', 'content', 'status',
-            'created_at', 'updated_at', 'author',
-            'category', 'tags'
-        )
+        fields = ('id', 'title', 'slug', 'content', 'status', 'created_at', 'updated_at', 'author', 'category', 'tags', 'comments_count')
+
+    def get_comments_count(self, obj):
+        return obj.comments.count()
+
+    def create(self, validated_data):
+        validated_data['slug'] = slugify(validated_data['title'])
+        return super().create(validated_data)
