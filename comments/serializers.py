7a860d15ce20483db2a76080fe62306adf6b1ff0
Samandar <samandarnayimjonov@gmail.com>
2025-02-23 17:27:27 +0500

    comment serializer update

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,9 +3,18 @@ from .models import Comment
 
 
 class CommentModelSerializer(serializers.ModelSerializer):
+    replies = serializers.SerializerMethodField()
+    author_email = serializers.EmailField(validators=[])
+
     class Meta:
         model = Comment
-        fields = (
-            'id', 'author_name', 'author_email', 'content',
-            'created_at', 'parent_comment', 'post'
-        )
+        fields = ('id', 'author_name', 'author_email', 'content', 'created_at', 'parent_comment', 'post', 'replies')
+
+    def get_replies(self, obj):
+        replies = Comment.objects.filter(parent_comment=obj.id)
+        return CommentModelSerializer(replies, many=True).data
+
+    def validate(self, data):
+        if data.get('parent_comment') and Comment.objects.filter(parent_comment=data['parent_comment']).count() >= 3:
+            raise serializers.ValidationError("Komment darajasi 3 dan oshmasligi kerak.")
+        return data
