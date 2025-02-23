155c7229916a92a0debdf1a48c3d67f6d57e071e
Samandar <samandarnayimjonov@gmail.com>
2025-02-23 17:26:36 +0500

    tagmodelserializer da slug fieldi auto qlindi

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 from authors.serializers import AuthorModelSerializer
 from .models import Post, Tag
+from django.utils.text import slugify
 
 
 class TagModelSerializer(serializers.ModelSerializer):
+    post_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Tag
-        fields = ('id', 'name', 'slug')
+        fields = ('id', 'name', 'slug', 'post_count')
+
+    def get_post_count(self, obj):
+        return obj.posts.count()
+
+    def create(self, validated_data):
+        validated_data['slug'] = slugify(validated_data['name'])
+        return super().create(validated_data)
+
 
 class PostModelSerializer(serializers.ModelSerializer):
     author = AuthorModelSerializer()
