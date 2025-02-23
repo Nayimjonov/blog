1fdc7bf309e8d117c9ee048936effc5a11ba509b
Samandar <samandarnayimjonov@gmail.com>
2025-02-23 17:37:56 +0500

    udpate serializer comment

--- comments/serializers.py
+++ comments/serializers.py
@@ -7,8 +7,7 @@ class CommentModelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ('id', 'author_name', 'author_email', 'content',
-                  'created_at', 'parent_comment', 'post', 'replies')
+        fields = ('id', 'author_name', 'author_email', 'content', 'created_at', 'parent_comment', 'post', 'replies')
 
     def get_replies(self, obj):
         if obj.replies.exists():
