bf491a1784e43ea42a3278f65cf1add0943cc752
Samandar <samandarnayimjonov@gmail.com>
2025-03-04 11:55:31 +0500

    done serializer post

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from django.utils.text import slugify
 from authors.serializers import AuthorModelSerializer
 from categories.serializers import CategoryModelSerializer
+from authors.models import Author
+from categories.models import Category
 from .models import Post, Tag
+
 
 
 class TagModelSerializer(serializers.ModelSerializer):
@@ -36,8 +39,7 @@ class PostModelSerializer(serializers.ModelSerializer):
     status = serializers.ChoiceField(choices=Post.STATUS_CHOICES)
     created_at = serializers.DateTimeField(read_only=True)
     updated_at = serializers.DateTimeField(read_only=True)
-    comments_count = serializers.IntegerField(source="comments.count", read_only=True)
-
+    comments_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
@@ -47,19 +49,25 @@ class PostModelSerializer(serializers.ModelSerializer):
             'tags', 'comments_count'
         )
 
-    def get_comments_count(self, obj):
-        return obj.comments.count()
-
     def create(self, validated_data):
+        tags_data = validated_data.pop('tags', [])
+        category_data = validated_data.pop('category', {})
+        author_data = validated_data.pop('author', {})
+
+        author, _ = Author.objects.get_or_create(**author_data)
+        validated_data['author'] = author
+
+        category, _ = Category.objects.get_or_create(**category_data)
+        validated_data['category'] = category
+
         if 'slug' not in validated_data:
             validated_data['slug'] = slugify(validated_data['title'])
-        return super().create(validated_data)
+        post = Post.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.content = validated_data.get('content', instance.content)
-        instance.status = validated_data.get('status', instance.status)
-        if 'title' in validated_data:
-            instance.slug = slugify(validated_data['title'])
-        instance.save()
-        return instance
+        for tag_data in tags_data:
+            tag_name = tag_data.get('name')
+            tag_slug = slugify(tag_name)
+
+            tag, _ = Tag.objects.get_or_create(slug=tag_slug, defaults={'name': tag_name})
+            post.tags.add(tag)
+        return post
