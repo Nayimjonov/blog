c66fa0608d719ef0736083fef0754ac2cebc7076
Samandar <samandarnayimjonov@gmail.com>
2025-03-06 13:08:57 +0500

    model serializer odddiyga uzgartrildi

--- authors/serializers.py
+++ authors/serializers.py
@@ -1,18 +1,8 @@
 from rest_framework import serializers
-from .models import Author
 
 
-class AuthorModelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = ('id', 'name', 'email', 'bio')
-
-    def validate_name(self, value):
-        if len(value) < 2:
-            raise serializers.ValidationError("Muallif ismi kamida 2 ta belgidan iborat bo'lishi kerak.")
-        return value
-
-    def validate_email(self, value):
-        if Author.objects.filter(email=value).exists():
-            raise serializers.ValidationError("Bu email manzil bilan ro'yxatdan o'tilgan.")
-        return value
+class AuthorSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+    email = serializers.EmailField()
+    bio = serializers.CharField()
