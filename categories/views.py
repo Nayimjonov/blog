d1df49f13f0d0f342c30260a7e59d92a55849bac
Samandar <samandarnayimjonov@gmail.com>
2025-03-11 10:44:12 +0500

    update view category

--- categories/views.py
+++ categories/views.py
@@ -1,45 +1,10 @@
-from rest_framework.views import APIView
-from django.http import Http404
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.generics import ListAPIView
+from .serializers import CategorySerializer
 from .models import Category
-from .serializers import CategoryModelSerializer
+from .pagination import CategoryListPagination
 
 
-class CategoryAPIView(APIView):
-    def get(self, request):
-        categories = Category.objects.all()
-        serializer = CategoryModelSerializer(categories, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CategoryModelSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class CategoryDetailAPIView(APIView):
-    def get_object(self, pk):
-        try:
-            return Category.objects.get(pk=pk)
-        except Category.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        category = self.get_object(pk)
-        serializer = CategoryModelSerializer(category)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        category = self.get_object(pk)
-        serializer = CategoryModelSerializer(category, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        category = self.get_object(pk)
-        category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class CategoryListView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = CategoryListPagination
