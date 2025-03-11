e1a10bc52bed2268117f2e83583fd21d2c93aa08
Samandar <samandarnayimjonov@gmail.com>
2025-03-11 10:48:00 +0500

    model udate

--- comments/models.py
+++ comments/models.py
@@ -3,12 +3,12 @@ from posts.models import Post
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author_name = models.CharField(max_length=255)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    parent_comment = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='child')
+    author_name = models.CharField(max_length=200)
     author_email = models.EmailField()
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    parent_comment = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
 
     def __str__(self):
-        return f"Comment by {self.author_name} on {self.post.title}"
+        return self.content
