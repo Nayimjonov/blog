e45a833edd6fcfe9081b4bb447ee0f7af1c26037
Samandar <samandarnayimjonov@gmail.com>
2025-03-11 11:10:18 +0500

    comment views updated

--- comments/views.py
+++ comments/views.py
@@ -1,45 +1,41 @@
-from rest_framework.views import APIView
-from django.http import Http404
 from rest_framework.response import Response
-from rest_framework import status
-from .models import Comment
-from .serializers import CommentModelSerializer
+from rest_framework.views import APIView
+from posts.models import Post
+from .pagination import CommentListPagination
+from .serializers import CommentSerializer
 
 
-class CommentAPIView(APIView):
-    def get(self, request):
-        comments = Comment.objects.all()
-        serializer = CommentModelSerializer(comments, many=True)
-        return Response(serializer.data)
+class CommentListCreateView(APIView):
+    pagination_class = CommentListPagination
 
-    def post(self, request):
-        serializer = CommentModelSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-
-class CommentDetailAPIView(APIView):
-    def get_object(self, pk):
+    def get_object(self, slug):
         try:
-            return Comment.objects.get(pk=pk)
-        except  Comment.DoesNotExist:
-            raise Http404
+            post = Post.objects.get(slug=slug)
+        except Post.DoesNotExist:
+            return Response({'error': 'Post Not Found'}, status=404)
+        return post
 
-    def get(self, request, pk):
-        comment = self.get_object(pk)
-        serializer = CommentModelSerializer(comment)
-        return Response(serializer.data)
+    def get(self, request, slug):
+        post = self.get_object(slug)
+        if isinstance(post, Response):
+            return post
 
-    def put(self, request, pk):
-        comment = self.get_object(pk)
-        serializer = CommentModelSerializer(comment, data=request.data)
+        comments = post.comments.all()
+        paginator = self.pagination_class()
+        result_page = paginator.paginate_queryset(comments, request)
+        serializer = CommentSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+    def post(self, request, slug):
+        post = self.get_object(slug)
+        if isinstance(post, Response):
+            return post
+
+        data = request.data.copy()
+        data['post'] = post.id
+
+        serializer = CommentSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        comment = self.get_object(pk)
-        comment.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
