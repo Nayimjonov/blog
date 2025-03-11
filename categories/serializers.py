29fbe81f2057a8edac13b028e9f14c67777ff874
Samandar <samandarnayimjonov@gmail.com>
2025-03-11 10:41:28 +0500

    category serializer update

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,26 +1,12 @@
 from rest_framework import serializers
-from .models import Category
-from django.utils.text import slugify
 
 
-class CategoryModelSerializer(serializers.ModelSerializer):
-    slug = serializers.CharField(required=False)
-    posts_count = serializers.IntegerField(source='posts.count', read_only=True)
+class CategorySerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    slug = serializers.SlugField()
+    description = serializers.CharField()
+    posts_count = serializers.SerializerMethodField()
 
-
-    class Meta:
-        model = Category
-        fields = ('id', 'name', 'slug', 'description', 'posts_count')
-
-    def create(self, validated_data):
-        if 'slug' not in validated_data:
-            validated_data['slug'] = slugify(validated_data['name'])
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        if 'name' in validated_data:
-            instance.slug = slugify(validated_data['name'])
-        instance.save()
-        return instance
+    def get_posts_count(self, obj):
+        return obj.posts.count()
